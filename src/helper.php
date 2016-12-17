7c72fab2ebf66ed5bf5a558928d163e29b2e619c
linchuangbin <451128520@qq.com>
2016-12-17 18:21:14 +0800

    更新方法
    更新方法

--- src/helper.php
+++ src/helper.php
@@ -17,10 +17,16 @@ function make_qrcode($content, $filename, $size = 4, $level = "L") {
  	if(empty($content)||empty($filename)){
  		return 'content或fieldname为空';
  	}
- 	import('qrcode.PHPQrcode.phpqrcode');
+ 	import('honray.tp5-qrcode.src.PHPQrcode.phpqrcode', VENDOR_PATH, '.php');
  	$QRcode = new \QRcode();
+
+ 	if(!is_dir('./uploads/qrcode/')){
+ 		@mkdir('./uploads/qrcode/');
+ 		@chmod('./uploads/qrcode/', 0777);
+ 	}
+
  	$path = "./uploads/qrcode/".$filename.".png";
  	$QRcode->png($content, $path, $level, $size);
- 	$url = "uploads/Qrcode/".$filename.".png";
+ 	$url = "uploads/qrcode/".$filename.".png";
  	return $url;
 }
